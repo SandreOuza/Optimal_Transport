f958e24840d016f535b851031defee3307de03b9
Andre Nogueira Souza <sandre@2379589954.vpn.umich.net>
2016-10-20 17:39:37 -0400

    changed more stuff. CHecking git really

--- importclass.py
+++ importclass.py
@@ -1,8 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-class Dog:
-    def __init__(self,name):
-        self.name = name
 
 class testclass:
     """This class tests the class structure and imports proper thigngs"""
@@ -19,7 +17,8 @@ class testclass:
         self.nu2 = 0   #the nusselt number computed via the optimality condition
         self.asp = 0   #the aspect ratio of the domain
         self.index = 0 # the import index
-        self.dt = 0    # size of timesteps being computed
+        self.dt1 = 0    # size of timesteps being computed, for the advec-diff
+        self.dt2 = 0    # size of timesteps being computed, for the optimality condition
         self.params = []
     def filetype(self, ftype):
         self.ftype = ftype
@@ -28,8 +27,38 @@ class testclass:
     def getparams(self):
         path = self.impdir + self.ftype + str(self.index) + '_params.txt'
         self.params = np.loadtxt(open(path,'r'))
+        self.mu = self.params[0]
+        self.asp = self.params[1]
+        self.xdim = int(self.params[2])
+        self.zdim = int(self.params[3])
+        self.dt1 = self.params[4]
+        self.dt2 = self.params[5]
+        self.pec1 = self.params[6]
+        self.nu1 = self.params[7]
+        self.pec2 = self.params[8]
+        self.nu2 = self.params[9]
+        self.resid = self.params[10]
+    def construct_grid(self):
+        self.xgrid = np.linspace(0,self.asp - 1.0/self.xdim,self.xdim)
+        self.zgrid = np.cos(np.linspace(0, np.pi, self.zdim))/2.0 + 0.5
+        self.Xgrid, self.Zgrid = np.meshgrid(self.xgrid, self.zgrid)
+    def import_field(self,field):
+        path = self.impdir + self.ftype + str(self.index) + field + '.txt'
+        self.field = np.loadtxt(open(path,'r'))
+    def plot_tempstream(self):
         
-    def explain_params():
+        path = self.impdir + self.ftype + str(self.index) + '_theta' + '.txt'
+        tmp = np.loadtxt(open(path,'r'))
+        thetafield = tmp.reshape(self.zdim, self.xdim)
+        path = self.impdir + self.ftype + str(self.index) + '_stream' + '.txt'
+        tmp = np.loadtxt(open(path,'r'))
+        streamfield = tmp.reshape(self.zdim, self.xdim)
+        pl1=plt.contourf(self.Xgrid, self.Zgrid, 1-self.Zgrid+0.5*thetafield, 513 , cmap = plt.cm.RdYlBu_r)
+        plt.colorbar(pl1, shrink=0.8, extend='both')
+        pl2=plt.contour(self.Xgrid, self.Zgrid, streamfield, 20, alpha=0.25, colors='k')
+        plt.show()
+        
+        
         
         
     
--- visualize.py
+++ visualize.py
@@ -15,8 +15,13 @@ def get_field(s1, s2):
     yy = y.reshape(257,128)
     return yy
 
+print('The absolute path to this directory is ')
+print(os.path.abspath(__file__) )
 abspath = '/Users/sandre/Desktop/variational_code/optimal_solver/DATA/'
-label = '0_100_theta.txt'
+family = "0_"
+index = "100_"
+field = "theta.txt"
+label = family+index+field
 yy = get_field(abspath, label)
 label = '0_100_stream.txt'
 ss = get_field(abspath, label)
@@ -35,7 +40,9 @@ plt.show()
 plt.contourf(X, Z, yy, cmap = plt.cm.gist_heat)
 plt.show()
 
-plt.contourf(X, Z, 1-Z+0.5*yy, 513 , cmap = plt.cm.RdYlBu_r)
+pl1=plt.contourf(X, Z, 1-Z+0.5*yy, 513 , cmap = plt.cm.RdYlBu_r)
+plt.colorbar(pl1, shrink=0.8, extend='both')
+pl2=plt.contour(X, Z, ss, 20, alpha=0.25, colors='k')
 plt.show()
 
 a = np.array([[1, 2, 3], [4, 5, 6]], float)
@@ -44,4 +51,15 @@ x = b[0:,0]
 y = b[0:,1]
 plt.plot(x,y,'ro')
 plt.axis([0, 6, 0, 20])
+plt.show()
+
+from importclass import testclass
+
+inst1 = testclass(abspath)
+inst1.filetype('0_')
+inst1.indexvalue(100)
+inst1.getparams()
+print(inst1.params)
+inst1.construct_grid()
+inst1.plot_tempstream()
 
