a7e8a829682a565d6311fd0561ecb72ad6324c6b
Andre Nogueira Souza <sandre@Andres-MacBook-Pro.local>
2016-10-29 14:03:14 -0400

    changed visualize.py and importclass.py

--- importclass.py
+++ importclass.py
@@ -28,7 +28,7 @@ class testclass:
     def getparams(self):
         path = self.impdir + self.ftype + str(self.index) + '_params.txt'
         self.params = np.loadtxt(open(path,'r'))
-        self.mu = self.params[0]
+        self.mu = self.params[0] #this is epsilon for optimal_a? family
         self.asp = self.params[1]
         self.xdim = int(self.params[2])
         self.zdim = int(self.params[3])
@@ -48,6 +48,7 @@ class testclass:
         self.field = np.loadtxt(open(path,'r'))
     def plot_tempstream(self):
         #other good colors for temp include plt.cm.coolwarm and
+        print('Plotting assuming theta exists')
         matplotlib.rcParams['contour.negative_linestyle'] = 'solid'
         path = self.impdir + self.ftype + str(self.index) + '_theta' + '.txt'
         tmp = np.loadtxt(open(path,'r'))
@@ -59,8 +60,56 @@ class testclass:
         plt.colorbar(pl1, shrink=0.8, extend='both')
         pl2=plt.contour(self.Xgrid, self.Zgrid, streamfield, 20, alpha=0.25, colors='k')
         plt.show()
-        
-        
+    def plot_tempstream2(self):
+        #other good colors for temp include plt.cm.coolwarm and
+        print('Plotting assuming sum exists')
+        matplotlib.rcParams['contour.negative_linestyle'] = 'solid'
+        #get sum
+        path = self.impdir + self.ftype + str(self.index) + '_sum' + '.txt'
+        tmp = np.loadtxt(open(path,'r'))
+        sumfield = tmp.reshape(self.zdim, self.xdim)
+        #get dif
+        path = self.impdir + self.ftype + str(self.index) + '_dif' + '.txt'
+        tmp = np.loadtxt(open(path,'r'))
+        diffield = tmp.reshape(self.zdim, self.xdim)
+        #get stream
+        path = self.impdir + self.ftype + str(self.index) + '_stream' + '.txt'
+        tmp = np.loadtxt(open(path,'r'))
+        streamfield = tmp.reshape(self.zdim, self.xdim)
+        #construct temp
+        #self.mu is eps and this needs to be divided by 4 to correspond to a 1.0 z-direction domain
+        #sumfield and diffield also need to be divided by two for the same reason
+        #the average of sumfield and theta field is what corresponds to theta
+        thetafield = 0.5*sumfield*np.sqrt(self.mu/4.0)
+        tempfield = (thetafield+diffield*0.5)*0.5
+        tempfield += 1.0-self.Zgrid
+        pl1=plt.contourf(self.Xgrid, self.Zgrid, tempfield, 513 , cmap = plt.cm.RdYlBu_r)
+        plt.colorbar(pl1, shrink=0.8, extend='both')
+        #pl2=plt.contour(self.Xgrid, self.Zgrid, streamfield, 20, alpha=0.25, colors='k')
+        plt.show()
+    def family_options(self):
+        print("----------------------------------------------------------------------------------")
+        print("0: Family name '0_', Optimal Transport solution where the aspect ratio is maximized")
+        print("1: Family name 'optimal_a0_', Optimal Transport solution with fundamental aspect ratio")
+        print("2: Family name 'optimal_a1_', Optimal Transport solution with (fundamental aspect ratio)*2^{-1}")
+        print("3: Family name 'optimal_a2_', Optimal Transport solution with (fundamental aspect ratio)*2^{-2}")
+        print("4: Family name 'optimal_a3_', Optimal Transport solution with (fundamental aspect ratio)*2^{-3}")
+        print("5: Family name 'optimal_a4_', Optimal Transport solution with (fundamental aspect ratio)*2^{-4}")
+        print("6: Family name 'optimal_a5_', Optimal Transport solution with (fundamental aspect ratio)*2^{-5}")
+        print("6: Family name 'optimal_a6_', Optimal Transport solution with (fundamental aspect ratio)*2^{-6}")
+        print("7: Family name 'optimal_a7_', Optimal Transport solution with (fundamental aspect ratio)*2^{-7}")
+        print("----------------------------------------------------------------------------------")
+    def family_set(self, num):
+        "Sets the ftype by using index values from family options. "
+        if num==0:
+            self.ftype = "0_"
+        if num>0 and num<8:
+            self.ftype = "optimal_a"+str(int(num))+"_"
+        else:
+            print('No such family. Check self.family_options for more options')
+
+    def get_scalings(self, num1, num2):
+        """Get the Nu-Pe scaligns from the params files over a range of indices """
         
         
     
--- visualize.py
+++ visualize.py
@@ -7,10 +7,13 @@ from importclass import testclass
 abspath = '/Users/sandre/Desktop/variational_code/optimal_solver/DATA/'
 inst1 = testclass(abspath)
 inst1.filetype('0_')
-
+inst1.family_set(1)
 inst1.indexvalue(100)
 inst1.getparams()
 print(inst1.params)
 inst1.construct_grid()
-inst1.plot_tempstream()
+inst1.plot_tempstream2()
+inst1.family_options()
+inst1.family_set(4)
+print(inst1.ftype)
 
